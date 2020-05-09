80d2da00e05b5ab159d8dad53fdc56bb6dbd0fdb
Rafael <rafaelgoncalvesbarreira@gmail.com>
2020-05-09 13:07:49 -0300

    creating unitTest

---
+++ ToDoList.Business.Test/Services/TaskServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Business.Contract.Infra;
+using ToDoList.Business.Contracts.Services;
+using Xunit;
+using NSubstitute;
+using ToDoList.Model;
+
+namespace ToDoList.Business.Test.Services
+{
+    public class TaskServiceTests
+    {
+        private TaskService _service;
+        private IRepository _mockRepository;
+        public TaskServiceTests()
+        {
+            _mockRepository = Substitute.For<IRepository>();
+
+            _service = new TaskService(_mockRepository);
+        }
+        [Fact]
+        public void CreateTaskTodoTest()
+        {
+            var task = new TaskToDo
+            {
+                Title = "Title",
+                Description = "Description",
+                Status = StatusEnum.Awaiting
+            };
+
+            _service.Create(task);
+
+            _mockRepository.ReceivedWithAnyArgs().Add<TaskToDo>(default);
+            _mockRepository.Received().Save();
+        }
+    }
+}
