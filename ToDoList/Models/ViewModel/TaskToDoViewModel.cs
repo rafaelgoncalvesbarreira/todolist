4bf74722a06b51df9f98c168bbd4fa1c5084df04
Rafael <rafaelgoncalvesbarreira@gmail.com>
2020-05-15 23:37:00 -0300

    adding id on task cards

--- ToDoList/Models/ViewModel/TaskToDoViewModel.cs
+++ ToDoList/Models/ViewModel/TaskToDoViewModel.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Mvc.Models.ViewModel
 {
     public class TaskToDoViewModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
     }
